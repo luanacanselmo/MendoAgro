6dc614f96f78b89cc664c22a4e017ec9f976bec6
Agustin Suarez <titoagustinsuarez6@gmail.com>
2024-05-27 21:16:49 -0300

    cambios en trabajo.php

--- vista/componentes/card.php
+++ vista/componentes/card.php
@@ -3,7 +3,7 @@ include("../modelo/conexion.php");
 
 $sql = $conexion->query("Select * from trabajos");
 while ($datos = $sql->fetch_object()) { ?>
-    <div class="block mb-3 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-1000">
+    <div class="block mb-3 p-6 bg-white border border-gray-200 rounded-lg shadow">
         <figure class="flex flex-row items-center space-x-2 mb-3">
             <img class="rounded-full w-5 h-5" src="https://ui-avatars.com/api/?name=Agustin+Suarez" alt="profile picture">
             <div class="font-medium text-left text-sm">Agustín Suarez</div>
@@ -12,6 +12,14 @@ while ($datos = $sql->fetch_object()) { ?>
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><?= $datos->titulo ?></h5>
             <p class="font-normal text-gray-700"><?= $datos->descripcion ?></p>
         </blockquote>
+        <div class="flex justify-end">
+            <button class="text-gray-700 text-sm flex items-center">Ver más<svg width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-down">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 5l0 14" />
+                    <path d="M16 15l-4 4" />
+                    <path d="M8 15l4 4" />
+                </svg></button>
+        </div>
         <!-- <figure class="flex items-center justify-center ">
         <figure/> -->
 
--- vista/trabajos.php
+++ vista/trabajos.php
@@ -9,20 +9,26 @@
 </head>
 
 <body>
-    <?php include"./componentes/nav.php";?>
-    <div id="app" class="h-screen z-10">
-        <section class="[template-area:side] bg-blue-200">
-            coso
+    <?php include "./componentes/nav.php"; ?>
+    <div id="app" class="h-screen z-10 bg-zinc-200 pt-5">
+        <section class="[template-area:side] ">
+
         </section>
         <main class="[template-area:main] grid grid-cols-5">
-            <div class="bg-zinc-200 col-span-3 pt-4 px-5">
+            <div class="col-span-3 px-5">
                 <h2 class="text-xl mb-4">Principales</h2>
                 <section>
-                    <?php include"./componentes/card.php"?>
+                    <?php include "./componentes/card.php" ?>
                 </section>
             </div>
-            <div class="bg-green-300 col-span-2">
-                coso
+            <div class="col-span-2 mr-2">
+                <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow">
+                    <div id="banner" class=" rounded-lg p-4 flex flex-col items-center justify-center space-y-2">
+                        <p class="text-xl font-semibold">¿Estás buscando Empleados?</p>
+                        <p>Puedes registrarte para empezar.</p>
+                        <input type="button" value="Resgistrase" class="rounded-lg bg-green-400 text-green-900 font-normal px-3 py-1 text-center">
+                    </div>
+                </div>
             </div>
         </main>
     </div>
@@ -32,11 +38,16 @@
 </html>
 
 <style>
-    #app{
+    #app {
         display: grid;
-		grid-template-areas:
-			"side main";
-		grid-auto-columns: 300px 1fr;
-		grid-template-rows: 1fr;
+        grid-template-areas:
+            "side main";
+        grid-auto-columns: 300px 1fr;
+        grid-template-rows: 1fr;
+    }
+
+    #banner{
+        background: url(../Icon-MendoAgro/banner.png);
+        background-size: cover;
     }
 </style>
