c66dd8378b612c98a11168d7c468996ffa871dde
Agustin Suarez <titoagustinsuarez6@gmail.com>
2024-05-27 21:37:31 -0300

    coso

--- vista/componentes/card.php
+++ vista/componentes/card.php
@@ -9,28 +9,39 @@ while ($datos = $sql->fetch_object()) { ?>
             <div class="font-medium text-left text-sm">Agustín Suarez</div>
         </figure>
         <blockquote>
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><?= $datos->titulo ?></h5>
-            <p class="font-normal text-gray-700"><?= $datos->descripcion ?></p>
+            <div class="mb-3">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><?= $datos->titulo ?></h5>
+                <p class="font-normal text-gray-700"><?= $datos->descripcion ?></p>
+            </div>
+            <div class="border-t-2 border-gray-200">
+                <ul>
+                    <li>
+                        <p class="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-currency-dollar">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
+                                <path d="M12 3v3m0 12v3" />
+                            </svg> <?= $datos->pago ?></p>
+                    </li>
+                    <li>
+                        <p class="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-phone">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                            </svg> <?= $datos->telefono ?></p>
+                    </li>
+                    <li>
+                        <p class="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clock-hour-8">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                                <path d="M12 12l-3 2" />
+                                <path d="M12 7v5" />
+                            </svg> <?= $datos->fechainicio ?> a <?= $datos->fechafinal ?></p>
+                    </li>
+                    <li>
+                        <p class="flex">Ubicación: <?= $datos->ubitrabajo ?>, <?= $datos->localidadtrabajo ?></p>
+                    </li>
+                </ul>
+            </div>
         </blockquote>
-        <div class="flex justify-end">
-            <button class="text-gray-700 text-sm flex items-center">Ver más<svg width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-down">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M12 5l0 14" />
-                    <path d="M16 15l-4 4" />
-                    <path d="M8 15l4 4" />
-                </svg></button>
-        </div>
-        <!-- <figure class="flex items-center justify-center ">
-        <figure/> -->
-
-
-        <!-- <p><?= $datos->descripcion ?></p>
-        <p><?= $datos->pago ?></p>
-        <p><?= $datos->telefono ?></p>
-        <p><?= $datos->fechainicio ?></p>
-        <p><?= $datos->fechafinal ?></p>
-        <p><?= $datos->localidadtrabajo ?></p>
-        <p><?= $datos->ubitrabajo ?></p> -->
     </div>
 <?php }
 ?>
